c97163c1585a0ee756422609437b5efb61559b47
ahmed7100 <ahmedsalim7100@gmail.com>
2025-07-13 07:49:40 +0100

    initial infrastructure setup using terraform

--- 01-provider.tf
+++ 01-provider.tf
@@ -22,22 +22,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-provider "kubernetes" {
-  alias                  = "eks"
-  host                   = aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
-}
-
 terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
       version = ">= 2.5.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.20.0"
     }
   }
 }
