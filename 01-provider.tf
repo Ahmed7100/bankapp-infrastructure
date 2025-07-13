5a8a9f19b283fb2cdc66ce94bd4ce95d64e4f1be
ahmed7100 <ahmedsalim7100@gmail.com>
2025-07-13 07:50:51 +0100

    initial infrastructure setup using terraform

--- 01-provider.tf
+++ 01-provider.tf
@@ -21,12 +21,3 @@ provider "namecheap" {
 provider "aws" {
   region = "us-east-1"
 }
-
-terraform {
-  required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.5.0"
-    }
-  }
-}
