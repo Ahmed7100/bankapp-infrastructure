03c5a33c77ce91c211d848edecb2e4f6193b10ed
ahmed7100 <ahmedsalim7100@gmail.com>
2025-07-13 06:29:27 +0100

    initial infrastructure setup using terraform

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -1,14 +1,16 @@
-provider "helm" {
-  kubernetes {
-    # This block is not supported and should be removed.
-  }
+provider "kubernetes" {
+  alias                   = "eks"
+  host                    = aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate  = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+  token                   = data.aws_eks_cluster_auth.eks.token
 }
 
-provider "kubernetes" {
-  alias                  = "eks"
-  host                   = aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
+provider "helm" {
+  kubernetes {
+    host                   = aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+  }
 }
 
 data "aws_eks_cluster_auth" "eks" {
