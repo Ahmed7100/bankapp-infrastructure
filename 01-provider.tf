583d202d51ff65b5b7706294ec23be55fd8190fb
ahmed7100 <ahmedsalim7100@gmail.com>
2025-07-13 08:32:01 +0100

    initial infrastructure

--- 01-provider.tf
+++ 01-provider.tf
@@ -1,23 +1,30 @@
 terraform {
   required_providers {
-    namecheap = {
-      source = "namecheap/namecheap"
-      version = "2.2.0"
-    }
+
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.0.0-beta2"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.20"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.10"
     }
   }
 }
 
-provider "namecheap" {
-  user_name = var.namecheap_username
-  api_user = var.namecheap_api_user
-  api_key = var.namecheap_api_key
-  client_ip = var.namecheap_client_ip
-  use_sandbox = false
-}
+# provider "namecheap" {
+#   user_name = var.namecheap_username
+#   api_user = var.namecheap_api_user
+#   api_key = var.namecheap_api_key
+#   client_ip = var.namecheap_client_ip
+#   use_sandbox = false
+#}
 provider "aws" {
   region = "us-east-1"
 }
